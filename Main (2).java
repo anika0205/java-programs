0d6e4e50095dc894ef818d3007a01369c9f45b76
anika0205 <anikamateti@gmail.com>
2025-06-13 11:32:06 +0530

    Add files via upload
    java

---
+++ Main (2).java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int a=34;
+		if (a>0){
+		    System.out.println("number is positive"+a);
+		    
+		}
+		else if(a<0){
+		    System.out.println("number is negative"+a);
+		    
+		}
+		else{
+		    System.out.println("numberis zero");
+		    
+		}
+	}
+}
